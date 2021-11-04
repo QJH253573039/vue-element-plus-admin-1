c833909297a17a94411417338790b651b5a754bd
cold <1104305558@qq.com>
2021-11-04 14:06:46 +0800

    配置默认网页标题-env

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
-
+let title = process.env.VUE_APP_TITLE
 module.exports = {
     lintOnSave: false,
     productionSourceMap: false,
 
     chainWebpack(config) {
+        config.plugin('html').tap(args=>{
+            args[0].title = title
+            return args
+        })
+
         config.module
             .rule('svg')
             .exclude.add(path.resolve(__dirname, './src/icons'))
